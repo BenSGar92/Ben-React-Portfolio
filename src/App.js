a0f66992aa094269a9d5218e648e44f40fbd7b2d
Ben G <bensgar92@gmail.com>
2021-01-18 12:42:24 -0700

    updated navbar for smaller screens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/navbar"
+import Navigation from "./components/navbar"
 import Main from "./pages/main/main"
 import Portfolio from "./pages/portfolio/portfolio"
 import Contact from "./pages/contact/contact"
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <div className="background">
-        <Navbar />
+        <Navigation />
           <Route exact path="/" component={Main} />
           <Route exact path="/main" component={Main} />
           <Route exact path="/portfolio" component={Portfolio} />
